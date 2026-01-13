1e8d4f351992173b1e41ed2972b767e8b4433bad
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2026-01-13 19:58:26 +0300

    Add product management features and update project structure
    - Updated BlazorWeb.csproj to include BlazorDisableThrowNavigationException property.
    - Enhanced NavMenu with a link to the Demo Product page.
    - Expanded BindProp.razor to include product properties and data binding.
    - Created DemoProduct.razor for displaying and managing product list.
    - Introduced Product_Prop model for product properties.
    - Updated _Imports.razor to include necessary namespaces.
    - Modified Product model to include ProductProperties.

--- BlazorWeb/Models/Product.cs
+++ BlazorWeb/Models/Product.cs
@@ -5,7 +5,8 @@ namespace BlazorWeb.Models;
 public class Product
 {
     public int Id { get; set; }
-    public string Name { get; set; }
+    public string Name { get; set; }=default!;
     public double Price { get; set; }
     public bool IsActive { get; set; }
+    public IEnumerable<Product_Prop> ProductProperties { get; set; }=new List<Product_Prop>();
 }
---
+++ BlazorWeb/Models/Product_Prop.cs
@@ -0,0 +1,8 @@
+namespace BlazorWeb.Models;
+
+public class Product_Prop
+{
+    public int Id { get; set; }
+    public string Key { get; set; } = default!;
+    public string Value { get; set; } = default!;
+}
