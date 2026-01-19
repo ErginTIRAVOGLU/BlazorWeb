aeccd1e0a9b3ae9e6ee57f48b93f239f8938de5f
Ergin TIRAVOGLU <ergintiravoglu@msn.com>
2026-01-19 19:51:26 +0300

    Add Blazor Forms page with product creation functionality and validation

--- BlazorWeb/Models/Product.cs
+++ BlazorWeb/Models/Product.cs
@@ -1,12 +1,27 @@
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace BlazorWeb.Models;
 
 public class Product
 {
     public int Id { get; set; }
+    [Required]
     public string Name { get; set; }=default!;
+    [Range(1,1000)]
     public double Price { get; set; }
     public bool IsActive { get; set; }
     public IEnumerable<Product_Prop> ProductProperties { get; set; }=new List<Product_Prop>();
+    public Category Category { get; set; }
+
+    public DateOnly AvailableAfter { get; set; }
+}
+
+public enum Category
+{
+    Electronics,
+    Clothing,
+    HomeAppliances,
+    Books,
+    Toys
 }
